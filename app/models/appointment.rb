2657eb2e3fdaa79d720013208433936de46f6c4a
nsellek <nick@seansellek.com>
2016-03-15 13:10:28 -0400

    added validators for first and last names and start and end times

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,7 @@
 class Appointment < ActiveRecord::Base
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :start_time, presence: true
+	validates :end_time, presence: true
+
 end
