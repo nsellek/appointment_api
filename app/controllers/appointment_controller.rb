84c61c506c1f9172be20963afb183e1dee68acf7
nsellek <nick@seansellek.com>
2016-03-15 13:58:54 -0400

    set up index to show all appointments

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -5,4 +5,22 @@ class AppointmentController < ApplicationController
 
 		render json: appointments, status: 200
 	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+	private
+
+	def valid_params
+		params.require(:appointment).permit(:first_name, :last_name, :start_time, :end_time)
+	end
 end
