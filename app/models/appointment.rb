d821b61afc8426f6688edcc2b10fe785f37021c7
nsellek <nick@seansellek.com>
2016-03-15 20:50:14 -0400

    fixed bugs where validations wouldnt work

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -13,7 +13,7 @@ class Appointment < ActiveRecord::Base
 		valid_month
 		valid_day
 		# valid_time
-		return false unless @valid = true
+		return false unless @valid == true
 		# valid_time_slot
 		@valid
 	end
@@ -26,27 +26,26 @@ class Appointment < ActiveRecord::Base
 	end
 
 	def valid_month
-
 		@number_of_month = 0
 		months = Date::MONTHNAMES
-		self.month.downcase.capitalize
+		# Normalizes the input
+		cap_month = self.month.downcase.capitalize
 
 		months.each_with_index do |name, index|
 			# Ignores the nil in the months array
 			if name
-				@number_of_month = index if self.month == name
-				@number_of_month = index if self.month == name[0..2]
-				@number_of_month = index if self.month.to_i == index
+				@number_of_month = index if cap_month == name
+				@number_of_month = index if cap_month == name[0..2]
+				@number_of_month = index if cap_month.to_i == index
 			end
 		end
 
 		@valid = false if @number_of_month == 0
-
 		if @valid && @current_year
 			# Checks to see if the month is the current one or one in the furture
-			@valid = false unless number_of_month >= Time.new.month
+			@valid = false unless @number_of_month >= Time.new.month
 			# Checks to see if month is current
-			@current_month = true if number_of_month == Time.new.month
+			@current_month = true if @number_of_month == Time.new.month
 		end
 	end
 
